789fff5dda0d613ea0f45380c98fb012bc434c38
theobeo2004 <thenhuet2004@gmail.com>
2025-04-19 22:53:32 +0700

    IntroExpressJS - feat: simple route

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express, { Router } from 'express'
 
 const app = express();
 
@@ -6,7 +6,17 @@ const PORT = 3000;
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.send('Hello, Express')
+    res.send('Hello, World')
+})
+
+// Define a simple route
+app.get('/about', (req, res) => {
+    res.send('This is about route')
+})
+
+// About Route
+app.get('/contact', (req, res) => {
+    res.send('This is contact route')
 })
 
 app.listen(PORT, () => {
