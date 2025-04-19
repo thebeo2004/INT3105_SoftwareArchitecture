83afa12ae8f392f2adbf4413291f1174e14f0bfe
theobeo2004 <thenhuet2004@gmail.com>
2025-04-19 23:11:02 +0700

    IntroExpressJS - feat: query string in route

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -9,9 +9,16 @@ app.get('/', (req, res) => {
     res.send('Hello, World')
 })
 
+// Define a dynamic route
 app.get('/user/:username', (req, res) => {
     const username = req.params.username;
     res.send(`Welcome ${username}`)
+})
+
+// Define a query route: search?keyword=express
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword;
+    res.send(`Searching for ${keyword}`);
 })
 
 app.listen(PORT, () => {
