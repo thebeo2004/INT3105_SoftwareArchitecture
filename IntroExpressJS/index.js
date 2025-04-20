b2c41e01c80fcdcbd1b12d66a1e4dc96fc71ad87
theobeo2004 <thenhuet2004@gmail.com>
2025-04-20 11:21:04 +0700

    IntroExpress JS - feat: templating engine with ejs which allows to directly embedde

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -5,20 +5,17 @@ const app = express();
 
 const PORT = 3000;
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs')
+
 // Define a simple route
 app.get('/', (req, res) => {
-    console.log('Middle')
-    res.send('Hello, World')
+    
+    const userName = 'John Doe'
+    res.render('index', {userName})
+
 })
 
-app.get('/error', () => {
-    throw new Error('This is test error')
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.send('Internal Server Error')
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
