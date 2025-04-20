d07de9c66772a7c3815455766ad547ce5b18aab9
theobeo2004 <thenhuet2004@gmail.com>
2025-04-20 10:42:54 +0700

    IntroExpressJS - feat: a simple middleware applied for the entire application

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -5,37 +5,16 @@ const app = express();
 
 const PORT = 3000;
 
+app.use((req, res, next) => {
+    console.log('A new req received at ' + Date.now());
+    next();
+})
+
 // Define a simple route
 app.get('/', (req, res) => {
     res.send('Hello, World')
 })
 
-app.use('/user', router)
-
-app.use(express.json())
-
-app.post('/users', (req, res) => {
-    const {name, email} = req.body 
-    res.json({
-        message: `User ${name} with email ${email} created successfully`
-    })
-})
-
-app.put('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const {name, email} = req.body 
-    res.json({
-        message: `User ${userId} updated to '${name}, ${email}`
-    })
-})
-
-app.delete('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    res.json({
-        messge: `User with ID ${userId} deleted successfully`
-    })
-})
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://locahost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
