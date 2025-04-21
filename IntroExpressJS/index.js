6f76c3f92d4a1668662e2f67db320b8a0d5f3f3f
theobeo2004 <thenhuet2004@gmail.com>
2025-04-21 16:42:10 +0700

    IntroExpressJS - feat: initialize session, and config parameters

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -1,31 +1,24 @@
 import express, { Router } from 'express'
 import cookieParser from 'cookie-parser'
+import session from 'express-session'
 
 const app = express()
 const PORT = 3000
 
 app.use(cookieParser())
+app.use(session({
+    secret: 'sample-secret',
+    resave: false,
+    saveUninitialized:false
+}))
 
 
 // Define a simple route
 app.get('/', (req, res) => {
-    
-    // Adding cookie exipry ~ 6 minutes
-    res.cookie('name', 'express-app', {maxAge: 360000})
-
     res.send('Hello Express')
-
 })
 
-app.get('/remove-cookie', (req, res) => {
-    res.clearCookie('name')
-    res.send('Cookie removed successfully')
-})
 
-app.get('/fetch', (req, res) => {
-    console.log(req.cookies)
-    res.send('API Called')
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
