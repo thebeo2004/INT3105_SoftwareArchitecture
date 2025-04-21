c5467ca8853d63bb6bbd91453ae5b76e78d71849
theobeo2004 <thenhuet2004@gmail.com>
2025-04-21 14:33:30 +0700

    IntroExpressJS - feat: config database schema and model

---
+++ IntroExpressJS/models/Person.js
@@ -0,0 +1,11 @@
+import mongoose  from "mongoose";
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age : Number,
+    email: String,
+
+})
+
+export const Person = mongoose.model("Person", personSchema)
+
