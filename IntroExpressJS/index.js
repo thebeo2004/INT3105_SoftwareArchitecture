6b08a8cfb92aca5b397edfac0be86d1019ca82e9
theobeo2004 <thenhuet2004@gmail.com>
2025-04-21 16:10:59 +0700

    IntroExpressJS - feat: initialize cookie & fetching stored cookie

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -1,51 +1,24 @@
 import express, { Router } from 'express'
-import { connectDB } from './config/db.js'
-import { Person } from './models/Person.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const PORT = 3000
 
-await connectDB()
+app.use(cookieParser())
+
 
 // Define a simple route
 app.get('/', (req, res) => {
     
+    res.cookie('name', 'express-app')
+
     res.send('Hello Express')
 
 })
 
-app.use(express.json())
-
-// Upload data
-app.post('/person', async (req, res) => {
-    const {email, name, age} = req.body
-    const newPerson = new Person({
-        name,
-        age, 
-        email
-    })
-    await newPerson.save()
-    console.log(newPerson)
-    res.send("Person added");
-})
-
-// Update data
-app.put('/person', async (req, res) => {
-    const {email} = req.body
-
-    const personData = await Person.find({
-        email
-    })
-
-    console.log(personData)
-    res.send('Update data successfully')
-})
-
-// Deleting Data from MongoDb
-app.delete('/person/:id', async (req, res) => {
-    const {id} = req.params
-    await Person.findByIdAndDelete(id)
-    res.send("Deleted object successfully")
+app.get('/fetch', (req, res) => {
+    console.log(req.cookies)
+    res.send('API Called')
 })
 
 app.listen(PORT, () => {
