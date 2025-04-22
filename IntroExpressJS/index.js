06f5ce0e66cc52eb7a431a78a7beb7f5851f9e20
theobeo2004 <thenhuet2004@gmail.com>
2025-04-22 15:04:53 +0700

    IntroExpressJS - feat: RESTful API with ExpressJS

--- IntroExpressJS/index.js
+++ IntroExpressJS/index.js
@@ -1,53 +1,49 @@
 import express, { Router } from 'express'
-import cookieParser from 'cookie-parser'
-import session from 'express-session'
 
 const app = express()
 const PORT = 3000
 
 // Define middleware
 app.use(express.json())
-app.use(cookieParser())
-app.use(session({
-    secret: 'sample-secret',
-    resave: false,
-    saveUninitialized:false
-}))
-
-
-const users = []
 
 // Define a simple route
 app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body
-    users.push({
-        username,
-        password
-    })
-    res.send('User registered successfully')
+// GET All Product
+app.get('/api/products', (req, res) => {
+    const products = [
+        {id: 1, name: 'Laptop', price: 1000},
+        {id: 2, name: 'Mobile', price: 500},
+    ]
+
+    res.status(200).json({products})
 })
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body
-    const user = users.find(u => u.username == username)
-    if (!user | password !== user.password) {
-        return res.send('Not authorized')
-    }
-    req.session.user = user
-    res.send('User Logged In')
+// Get a Single Product
+app.get('/api/products/:id', (req, res) => {
+    const products = [
+        {id: 1, name: 'Laptop', price: 1000},
+        {id: 2, name: 'Mobile', price: 500},
+    ]
+
+    const product = products.find(p => p.id === Number(req.params.id))
+
+    if (!product) {
+        return res.status(404).json({message: 'Product Not Found'})
+    } 
+
+    res.status(200).json({product})
+
 })
 
-app.get('/dashboard', (req, res) => {
-    if (!req.session.user) {
-        return res.send('Unauthorized')
-    }
-    res.send(`Welcome ${req.session.user.username}`)
+//Create a new product
+app.post('/api/products', (req, res) => {
+    const newProduct = req.body 
+    newProduct.id = Date.now()
+    res.status(201).json({newProduct})
 })
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
