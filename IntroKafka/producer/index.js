f8d341b432c7f5099c41f6dd195436889216d75c
theobeo2004 <thenhuet2004@gmail.com>
2025-04-24 22:49:37 +0700

    IntroKafka - feat: implement producer connected to Kafka

--- IntroKafka/producer/index.js
+++ IntroKafka/producer/index.js
@@ -1,1 +1,25 @@
-console.log('producer..')
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092']
+});
+
+const producer = kafka.producer();
+
+const runProducer = async () => {
+    await producer.connect();
+
+    await producer.send({
+        topic: 'test-topic',
+        messages: [{
+            value: "Hello Kafka from Node.js!"
+        }]
+    });
+
+    console.log('Message sent successfully');
+
+    await producer.disconnect();
+}
+
+runProducer().catch(console.error);
