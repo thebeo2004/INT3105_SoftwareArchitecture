405b53c101a87a9925618bc06e638acf6048324d
theobeo2004 <thenhuet2004@gmail.com>
2025-04-30 21:15:34 +0700

    IntroPrometheus - feat: RESTful API for randomly flipping the coin

---
+++ IntroPrometheus/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/flip-coints', (req, res) => {
+    const times = req.query.times;
+
+    if (times && times > 0) {
+        let heads = 0;
+        let tails = 0;
+
+        for (let i = 0; i < times; i++) {
+            let randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                heads++;
+            } else {
+                tails++;
+            }
+        }
+
+        res.json({heads, tails});
+    } else {
+        res.send('Hello! I work!!')
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Started Server. Server is runnint at PORT ${PORT}`);
+})
