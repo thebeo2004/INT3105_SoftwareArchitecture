6ab2f11b589582a33546f92f656ffaf0f062f653
theobeo2004 <thenhuet2004@gmail.com>
2025-05-01 17:03:02 +0700

    IntroPrometheus - feat: add HttpRequestTimer metric

--- IntroPrometheus/server.js
+++ IntroPrometheus/server.js
@@ -1,5 +1,5 @@
 import express, { response } from "express";
-import { Registry, Counter, collectDefaultMetrics } from "prom-client";
+import { Registry, Counter, collectDefaultMetrics, Histogram } from "prom-client";
 
 const app = express();
 const PORT = 3000;
@@ -25,13 +25,26 @@ register.registerMetric(headsCount);
 register.registerMetric(tailsCount);
 register.registerMetric(flipsCount);
 
-register.setDefaultLabels({
-    app: 'coin-api'
-});
+collectDefaultMetrics({
+    app: 'coin-api',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // How big the buckets should be for the Garbage Collection Histogram
+    register})
 
-collectDefaultMetrics({register})
+// Create a custom histogram metric
+const httpRequestTimer = new Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 0.3, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
+})
+
+register.registerMetric(httpRequestTimer)
 
 app.get('/flip-coints', (req, res) => {
+    // Start the HTTP request timer
+    const end = httpRequestTimer.startTimer();
+    const route = req.route.path;
     const times = req.query.times;
 
     if (times && times > 0) {
@@ -54,6 +67,8 @@ app.get('/flip-coints', (req, res) => {
     } else {
         res.send('Hello! I work!!')
     }
+
+    end({route, code: res.statusCode, method: req.method})
 });
 
 app.get('/metrics', async (req, res) => {
