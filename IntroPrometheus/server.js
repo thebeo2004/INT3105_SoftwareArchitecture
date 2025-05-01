bac3cb83b36fef3a6f45e36e2256e8a0712465d3
theobeo2004 <thenhuet2004@gmail.com>
2025-05-01 11:00:19 +0700

    IntroPrometheus - feat: custom metrics for server monitoring

--- IntroPrometheus/server.js
+++ IntroPrometheus/server.js
@@ -1,7 +1,35 @@
-import express from "express";
+import express, { response } from "express";
+import { Registry, Counter, collectDefaultMetrics } from "prom-client";
 
 const app = express();
 const PORT = 3000;
+
+const register = new Registry();
+
+const headsCount = new Counter({
+    name: "heads_count",
+    help: "Number of heads"
+});
+
+const tailsCount = new Counter({
+    name: "tails_count",
+    help: "Number of tails"
+});
+
+const flipsCount = new Counter({
+    name: "flips_count",
+    help: "Number of flips"
+});
+
+register.registerMetric(headsCount);
+register.registerMetric(tailsCount);
+register.registerMetric(flipsCount);
+
+register.setDefaultLabels({
+    app: 'coin-api'
+});
+
+collectDefaultMetrics({register})
 
 app.get('/flip-coints', (req, res) => {
     const times = req.query.times;
@@ -24,6 +52,11 @@ app.get('/flip-coints', (req, res) => {
         res.send('Hello! I work!!')
     }
 });
+
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-type', register.contentType);
+    res.end(await register.metrics());
+})
 
 app.listen(PORT, () => {
     console.log(`Started Server. Server is running at PORT ${PORT}`);
